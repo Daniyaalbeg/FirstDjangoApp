2d9c7674408fb2c4a386294587af74b34f5988b2
ec16467 <d.beg@se16.qmul.ac.uk>
2018-10-13 18:47:16 +0100

    Added First template view with 4 stub views

--- polls/urls.py
+++ polls/urls.py
@@ -4,4 +4,10 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name='index'),
+    #/polls/5/
+    path('<int:question_id>/', views.detail, name = 'detail'),
+    #/polls/5/results
+    path('<int:question_id>/results/', views.results, name = 'results'),
+    #/polls/5/vote
+    path('<intLquestion_id>/vote/', views.vote, name = 'vote'),
 ]
--- polls/views.py
+++ polls/views.py
@@ -2,6 +2,30 @@ from django.shortcuts import render
 
 # Create your views here.
 from django.http import HttpResponse
+from django.template import loader
+from .models import Question
 
+#Full index function compared to shorter on at the bottom
 def index(request):
-    return HttpResponse("Hello, world.. You are at the polls index.")
+    latest_question_list = Question.objects.order_by('-pub_date')[:5]
+    template = loader.get_template('polls/index.html')
+    context = {
+        'latest_question_list' : latest_question_list,
+    }
+    return HttpResponse(template.render(context, request))
+
+def detail(request, question_id):
+    return HttpResponse("You are looking at question %s." % question_id)
+
+def results(request, question_id):
+    response = "You are looking at results for question number %s."
+    return HttpResponse(response % question_id)
+
+def vote(request, question_id):
+    return HttpResponse("You are voting on question %s." % question_id)
+
+
+#def index(request):
+#    latest_question_list = Question.objects.order_by('-pub_date')[:5]
+#    context = {'latest_question_list': latest_question_list}
+#    return render(request, 'polls/index.html', context)
