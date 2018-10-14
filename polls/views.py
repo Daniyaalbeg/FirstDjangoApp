720fee03286f643641ea4ac6676a1b1e16cd8c17
daniyaal <danyaalbeg@gmail.com>
2018-10-15 00:22:52 +0100

    Added new detail view

--- polls/views.py
+++ polls/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import get_object_or_404, render
 
 # Create your views here.
 from django.http import HttpResponse
@@ -15,12 +15,10 @@ def index(request):
     }
     return HttpResponse(template.render(context, request))
 
+#Full detail function commented below
 def detail(request, question_id):
-    try:
-        question = Question.objects.get(pk = question_id)
-        context = {'question' : question}
-    except Question.DoesNotExsist:
-        raise Http404("Question does not exist")
+    question = get_object_or_404(Question, pk=question_id)
+    context = { 'question' : question}
     return render(request, 'polls/detail.html', context)
 
 
@@ -36,3 +34,12 @@ def vote(request, question_id):
 #    latest_question_list = Question.objects.order_by('-pub_date')[:5]
 #    context = {'latest_question_list': latest_question_list}
 #    return render(request, 'polls/index.html', context)
+
+
+#def detail(request, question_id):
+#    try:
+#        question = Question.objects.get(pk = question_id)
+#        context = {'question' : question}
+#    except Question.DoesNotExsist:
+#        raise Http404("Question does not exist")
+#    return render(request, 'polls/detail.html', context)
