16896c210560741b6b03786b2f17521371a4897f
ec16467 <d.beg@se16.qmul.ac.uk>
2018-10-15 20:21:30 +0100

    Added voting function and results page

--- polls/urls.py
+++ polls/urls.py
@@ -2,6 +2,7 @@ from django.urls import path
 
 from . import views
 
+app_name = 'polls'
 urlpatterns = [
     path('', views.index, name='index'),
     #/polls/5/
@@ -9,5 +10,5 @@ urlpatterns = [
     #/polls/5/results
     path('<int:question_id>/results/', views.results, name = 'results'),
     #/polls/5/vote
-    path('<intLquestion_id>/vote/', views.vote, name = 'vote'),
+    path('<int:question_id>/vote/', views.vote, name='vote'),
 ]
